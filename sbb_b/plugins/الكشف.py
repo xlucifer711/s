55f86a12b094d89b1fc759e6c6823379a0f39eae
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-10-17 00:02:54 +0200

    Add files via upload

---
+++ sbb_b/plugins/الكشف.py
@@ -0,0 +1,154 @@
+import html
+import os
+import base64
+
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.tl.types import MessageEntityMentionName
+
+from requests import get
+from telethon.tl.functions.photos import GetUserPhotosRequest
+from telethon.tl.functions.users import GetFullUserRequest
+
+from sbb_b import sbb_b
+from sbb_b.core.logger import logging
+
+from sbb_b.Config import Config
+from sbb_b.core.managers import edit_or_reply, edit_delete
+from sbb_b.helpers import reply_id
+from sbb_b.sql_helper.globals import gvarstatus
+from sbb_b.plugins import spamwatch
+
+LOGS = logging.getLogger(__name__)
+
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
+
+async def fetch_info(replied_user, event):
+    """Get details from the User object."""
+    FullUser = (await event.client(GetFullUserRequest(replied_user.id))).full_user
+    replied_user_profile_photos = await event.client(
+        GetUserPhotosRequest(user_id=replied_user.id, offset=42, max_id=0, limit=80)
+    )
+    replied_user_profile_photos_count = "⌔∮ هذا المستخدم لم يضع اي صورة"
+    dc_id = "Can't get dc id"
+    try:
+        replied_user_profile_photos_count = replied_user_profile_photos.count
+        dc_id = replied_user.photo.dc_id
+    except AttributeError:
+        pass
+    user_id = replied_user.id
+    first_name = replied_user.first_name
+    full_name = FullUser.private_forward_name
+    common_chat = FullUser.common_chats_count
+    username = replied_user.username
+    user_bio = FullUser.about
+    is_bot = replied_user.bot
+    restricted = replied_user.restricted
+    verified = replied_user.verified
+    photo = await event.client.download_profile_photo(
+        user_id,
+        Config.TMP_DOWNLOAD_DIRECTORY + str(user_id) + ".jpg",
+        download_big=True,
+    )
+    first_name = (
+        first_name.replace("\u2060", "")
+        if first_name
+        else ("هذا المستخدم ليس لديه اسم اول")
+    )
+    full_name = full_name or first_name
+    username = "@{}".format(username) if username else ("⌔∮ هذا المستخدم ليس لديه معرف")
+    user_bio = "⌔∮ هذا المستخدم ليس لديه اي نبذة" if not user_bio else user_bio
+    rozrtba = (
+        ".「  مآلُِڪ آلُِسورس 𓄂𓆃 」."
+        if user_id == 1355571767 or user_id == 1050898456 or user_id == 1001132193 or user_id == 1099460779 or user_id == 627658332 or user_id == 1833610203
+        else (".「  العضـو 𓅫 」.")
+    )
+    rozrtba = (
+        ".「 مـالك الحساب  」."
+        if user_id == (await event.client.get_me()).id
+        and user_id != 1833610203
+        and user_id != 627658332
+        and user_id != 1099460779
+        and user_id != 1355571767
+        and user_id != 1050898456
+        and user_id != 1001132193
+        else rozrtba
+    )    
+    caption += f"<b>╮•⎚ مـعلومات الـشخص مـن بـوت كرستين</b>\n"
+    caption += f"<b> الاسـم    ⇦ </b> {first_name} {last_name}\n"
+    caption += f"<b> المعـرف  ⇦ </b> {username}\n"
+    caption += f"<b> الايـدي   ⇦ </b> <code>{user_id}</code>\n"
+    caption += f"<b> الرتبـــه  ⇦ {rotbat} </b>\n"
+    caption += f"<b> الصـور   ⇦ </b> {replied_user_profile_photos_count}\n"
+    caption += f"<b> الحسـاب ⇦ </b> "
+    caption += f'<a href="tg://user?id={user_id}">{first_name}</a>'
+    caption += f"\n<b> الـمجموعات المشتـركة ⇦ </b> {common_chat} \n"
+    caption += f"<b> البايـو    ⇦ </b> {user_bio} \n"
+    caption += f"<b> ٴ•━─━ََِِ𝗰ًًٍٍ𝗥ٍٍٖٔ𝗧ََِِ𝗲ّّ𝗮ِِّّ𝗺━─━• </b>\n"
+    caption += f"<b> 𓆩 ََِِ𝗰.ًًٍٍ𝗥 ََِِ𝘀ُُ𝗼ًًٍٍ𝗨ًًٍٍ𝗥ََِِ𝗰ََِِ𝗲 𓆪 </b> - @cr_source"
+    return photo, caption
+
+@sbb_b.ar_cmd(pattern="ايدي(?: |$)(.*)")
+async def who(event):
+    roz = await edit_or_reply(event, "⇆")
+    if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+    replied_user = await get_user_from_event(event)
+    try:
+        photo, caption = await fetch_info(replied_user, event)
+    except AttributeError:
+        return await edit_or_reply(roz,  "**⌔∮ لم يتم العثور على معلومات لهذا المستخدم **")
+    message_id_to_reply = event.message.reply_to_msg_id
+    if not message_id_to_reply:
+        message_id_to_reply = None
+    try:
+        await event.client.send_file(
+            event.chat_id,
+            photo,
+            caption=caption,
+            link_preview=False,
+            force_document=False,
+            reply_to=message_id_to_reply,
+            parse_mode="html",
+        )
+        if not photo.startswith("http"):
+            os.remove(photo)
+        await roz.delete()
+    except TypeError:
+        await roz.edit(caption, parse_mode="html")
+
+
+@sbb_b.ar_cmd(pattern="رابط الحساب(?:\s|$)([\s\S]*)")
+async def permalink(mention):
+    user, custom = await get_user_from_event(mention)
+    if not user:
+        return
+    if custom:
+        return await edit_or_reply(mention, f"[{custom}](tg://user?id={user.id})")
+    tag = user.first_name.replace("\u2060", "") if user.first_name else user.username
+    await edit_or_reply(mention, f"⪼  [{tag}](tg://user?id={user.id})  𓆰. ")
