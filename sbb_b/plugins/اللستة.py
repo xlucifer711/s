bdf1d996fd70c6e63e226530347701d8d515e950
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-10-19 00:43:58 +0200

    Update اللستة.py

--- sbb_b/plugins/اللستة.py
+++ sbb_b/plugins/اللستة.py
@@ -2,7 +2,7 @@ from ..helpers.functions.functions import make_inline
 from . import edit_delete, reply_id, sbb_b
 
 
-@sbb_b.ar_cmd(pattern="لستة(?:\s|$)([\s\S]*)")
+@sbb_b.ar_cmd(pattern="التنصيب(?:\s|$)([\s\S]*)")
 async def _(event):
     reply_to_id = await reply_id(event)
     reply_message = await event.get_reply_message()
@@ -11,6 +11,6 @@ async def _(event):
     else:
         markdown_note = "".join(event.text.split(maxsplit=1)[1:])
     if not markdown_note:
-        return await edit_delete(event, "**- يجب عليك كتابة الامر بشكل صحيح**")
+        return await edit_delete(event, "**-https://t.me/cr_source/5596 **")
     await make_inline(markdown_note, event.client, event.chat_id, reply_to_id)
     await event.delete()
