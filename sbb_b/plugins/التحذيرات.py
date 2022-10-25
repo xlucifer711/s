14958c94c222076f820dc4a01ecfa0bd001980a3
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-10-25 20:09:45 +0200

    Update التحذيرات.py

--- sbb_b/plugins/التحذيرات.py
+++ sbb_b/plugins/التحذيرات.py
@@ -1,16 +1,27 @@
 import html
 
-from sbb_b import sbb_b
+from sbb_b import sbb_b 
 
 from ..core.managers import edit_or_reply
 from ..sql_helper import warns_sql as sql
 
+plugin_category = "admin"
 
-@sbb_b.ar_cmd(pattern="تحذير(?:\s|$)([\s\S]*)")
+
+@sbb_b.ar_cmd(
+    pattern="تحذير(?:\s|$)([\s\S]*)",
+    command=("تحذير", plugin_category),
+    info={
+        "header": "لتحذير المستخدم.",
+        "description": "سيحذر المستخدم الذي تم الرد عليه.",
+        "usage": "تحذير <السبب>",
+    },
+)
 async def _(event):
+    "لتحذير المستخدم"
     warn_reason = event.pattern_match.group(1)
     if not warn_reason:
-        warn_reason = "⪼ لا يوجد سبب ، 🗒"
+        warn_reason = "- لا يوجد سبب ، 🗒"
     reply_message = await event.get_reply_message()
     limit, soft_warn = sql.get_warn_setting(event.chat_id)
     num_warns, reasons = sql.warn_user(
@@ -19,12 +30,12 @@ async def _(event):
     if num_warns >= limit:
         sql.reset_warns(reply_message.sender_id, event.chat_id)
         if soft_warn:
-            logger.info("TODO: طرد المستخدم")
+            logger.info("TODO: kick user")
             reply = "**▸┊بسبب تخطي التحذيرات الـ {} ، يجب طرد المستخدم! 🚷**".format(
                 limit, reply_message.sender_id
             )
         else:
-            logger.info("TODO: حظر المستخدم")
+            logger.info("TODO: ban user")
             reply = "**▸┊بسبب تخطي التحذيرات الـ {} ، يجب حظر المستخدم! ⛔️**".format(
                 limit, reply_message.sender_id
             )
@@ -37,8 +48,16 @@ async def _(event):
     await edit_or_reply(event, reply)
 
 
-@sbb_b.ar_cmd(pattern="التحذيرات")
+@sbb_b.ar_cmd(
+    pattern="التحذيرات",
+    command=("التحذيرات", plugin_category),
+    info={
+        "header": "للحصول على قائمة تحذيرات المستخدمين.",
+        "usage": "التحذير <بالرد>",
+    },
+)
 async def _(event):
+    "للحصول على قائمة تحذيرات المستخدمين."
     reply_message = await event.get_reply_message()
     if not reply_message:
         return await edit_delete(
@@ -52,24 +71,27 @@ async def _(event):
     if not reasons:
         return await edit_or_reply(
             event,
-            "**▸┊[ المستخدم 👤](tg://user?id={}) **لديه {}/{} تحذيرات ، لكن لا توجد اسباب !**".format(
-                num_warns, limit
-            ),
+            f"▸┊هذا المستخدم لديه {num_warns} / {limit} تحذيرات ، لكن لا توجد اسباب !",
         )
-
-    text = "**▸┊[ المستخدم 👤](tg://user?id={}) **لديه {}/{} تحذيرات ، للأسباب : ↶**".format(
-        num_warns, limit
-    )
-
-    text = "**▸┊ المستخدم لديه {}/{} تحذيرات ، للأسباب : ↶:".format(num_warns, limit)
+    text = f"▸┊هذا المستخدم لديه {num_warns} / {limit} تحذيرات ، للأسباب : ↶"
     text += "\r\n"
     text += reasons
     await event.edit(text)
 
 
-@sbb_b.ar_cmd(pattern="حذف التحذيرات(?: |$)(.*)")
+@sbb_b.ar_cmd(
+    pattern="ح(ذف) ?التحذير$",
+    command=("حذف التحذير", plugin_category),
+    info={
+        "header": "لحذف تحذيرات المستخدم الذي تم الرد عليه",
+        "usage": [
+            "{tr}ح التحذير",
+            "{tr}حذف التحذير",
+        ],
+    },
+)
 async def _(event):
+    "لحذف او اعادة تحذيرات المستخدم الذي تم الرد عليه"
     reply_message = await event.get_reply_message()
     sql.reset_warns(reply_message.sender_id, event.chat_id)
     await edit_or_reply(event, "**▸┊تم إعادة ضبط التحذيرات!**")
-
