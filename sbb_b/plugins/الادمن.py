e5db14e0f19fb7fc8e24abf5b4c57d55c7c80d28
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-10-26 00:40:44 +0200

    Update الادمن.py

--- sbb_b/plugins/الادمن.py
+++ sbb_b/plugins/الادمن.py
@@ -241,9 +241,7 @@ async def endmute(event):
     if not user:
         return
     if user.id == 1355571767:
-           return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
-    if user.id == 1050898456:
-        return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
+           return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
     catevent = await edit_or_reply(event, "⌯︙يـتم طـرد الـمستخدم أنتـظر")
     try:
         await event.client.kick_participant(event.chat_id, user.id)
@@ -276,9 +274,7 @@ async def _ban_person(event):
     if not user:
         return
     if user.id == 1355571767:
-           return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
-    if user.id == 1050898456:
-           return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
+           return await edit_delete(event, "- لا يمڪنني حظر مطـوري دي لك")
     catevent = await edit_or_reply(event, "⌯︙تـم حـظره بـنجاح")
     try:
         await event.client(EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS))
