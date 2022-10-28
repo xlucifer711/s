6ca25566693981c3b8fed4fb14f4998d3bb2443a
xlucifer711 <106505627+xlucifer711@users.noreply.github.com>
2022-10-28 08:35:47 +0200

    Update تحشيش.py

--- sbb_b/plugins/تحشيش.py
+++ sbb_b/plugins/تحشيش.py
@@ -1,22 +1,12 @@
-import html
-import os
+import random
 
-from requests import get
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.utils import get_input_location
+from razan.strings.fun import *
+from sbb_b import sbb_b
+from sbb_b.core.managers import edit_or_reply
+from sbb_b.helpers import get_user_from_event
 
-from sbb_b import sbb_b 
-from razan.strings import *
-from telethon import events
-from ..Config import Config
-from ..core.managers import edit_or_reply
-from ..helpers import get_user_from_event, reply_id
-from . import spamwatch
-from telethon.utils import get_display_name
-from ..helpers.utils import reply_id, _catutils, parse_pre, yaml_format, install_pip, get_user_from_event, _format
+from . import *
 
-plugin_category = "utils"
 
 
 
